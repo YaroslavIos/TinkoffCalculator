4d92e7aac88dc879047140d3032ff222196ebfa1
Snow37 <110422108+Snow37@users.noreply.github.com>
2024-02-20 20:18:23 +0300

    Connect buttons between StoryBoard and ViewController.

---
+++ TinkoffCalculator/ViewController.swift
@@ -0,0 +1,141 @@
+//
+//  ViewController.swift
+//  TinkoffCalculator
+//
+//  Created by Ярослав Любиченко on 20.2.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBAction func zeroButton(_ sender: UIButton) {
+        guard let buttonTitle = sender.currentTitle else {
+            return
+        }
+        print(buttonTitle)
+    }
+    
+    @IBAction func oneButton(_ sender: UIButton) {
+        guard let buttonTitle = sender.currentTitle else {
+            return
+        }
+        print(buttonTitle)
+    }
+    
+    @IBAction func secondButton(_ sender: UIButton) {
+        guard let buttonTitle = sender.currentTitle else {
+            return
+        }
+        print(buttonTitle)
+    }
+    
+    
+    @IBAction func thirdButton(_ sender: UIButton) {
+        guard let buttonTitle = sender.currentTitle else {
+            return
+        }
+        print(buttonTitle)
+    }
+    
+    @IBAction func forthButton(_ sender: UIButton) {
+        guard let buttonTitle = sender.currentTitle else {
+            return
+        }
+        print(buttonTitle)
+    }
+    
+    @IBAction func fifthButton(_ sender: UIButton) {
+        guard let buttonTitle = sender.currentTitle else {
+            return
+        }
+        print(buttonTitle)
+    }
+    
+    @IBAction func sixthButton(_ sender: UIButton) {
+        guard let buttonTitle = sender.currentTitle else {
+            return
+        }
+        print(buttonTitle)
+    }
+    
+    @IBAction func seventhButton(_ sender: UIButton) {
+        guard let buttonTitle = sender.currentTitle else {
+            return
+        }
+        print(buttonTitle)
+    }
+    
+    @IBAction func eighthButton(_ sender: UIButton) {
+        guard let buttonTitle = sender.currentTitle else {
+            return
+        }
+        print(buttonTitle)
+    }
+    
+    @IBAction func ninthButton(_ sender: UIButton) {
+        guard let buttonTitle = sender.currentTitle else {
+            return
+        }
+        print(buttonTitle)
+    }
+    
+    @IBAction func clearButton(_ sender: UIButton) {
+        guard let buttonTitle = sender.currentTitle else {
+            return
+        }
+        print(buttonTitle)
+    }
+    
+    @IBAction func dividerButton(_ sender: UIButton) {
+        guard let buttonTitle = sender.currentTitle else {
+            return
+        }
+        print(buttonTitle)
+    }
+    
+    @IBAction func multiplyButton(_ sender: UIButton) {
+        guard let buttonTitle = sender.currentTitle else {
+            return
+        }
+        print(buttonTitle)
+    }
+    
+    @IBAction func minusButton(_ sender: UIButton) {
+        guard let buttonTitle = sender.currentTitle else {
+            return
+        }
+        print(buttonTitle)
+    }
+    
+    
+    @IBAction func plusButton(_ sender: UIButton) {
+        guard let buttonTitle = sender.currentTitle else {
+            return
+        }
+        print(buttonTitle)
+    }
+    
+    @IBAction func equalButton(_ sender: UIButton) {
+        guard let buttonTitle = sender.currentTitle else {
+            return
+        }
+        print(buttonTitle)
+    }
+    
+    
+    @IBAction func comaButton(_ sender: UIButton) {
+        guard let buttonTitle = sender.currentTitle else {
+            return
+        }
+        print(buttonTitle)
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
